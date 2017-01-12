9fdaf21019849fbc81a1c5782119c13d71f0e5b1
Damien <damien@izettle.com>
2017-01-12 19:12:08 +0100

    Added raven

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import "whatwg-fetch"
 import React from "react"
 import ReactDOM from "react-dom"
 import Formsy from "formsy-react"
+import Raven from "raven-js"
 
 import App from "app"
 
 import "./style/all.scss"
+
+if (window.host !== undefined) {
+  Raven.config("https://9515837d73e94b2e9f4dcad3d7b6e443@sentry.io/128809").install()
+}
 
 Formsy.addValidationRule("isPresent", (values, value) => (
   value !== null && value !== "" && value !== undefined
